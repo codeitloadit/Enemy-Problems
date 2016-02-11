16c9947fc4fe7d2c569433d1cc05b07979a9be9a
codeitloadit <brian@codeitload.it>
2016-02-11 01:04:15 -0800

    Initial RevMob banner

---
+++ Assets/Scripts/AdsRevMob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class AdsRevMob : MonoBehaviour {
+	private static readonly Dictionary<String, String> REVMOB_APP_IDS = new Dictionary<String, String>() {
+		{ "Android", "56bc43f91c64102a6106ac10" },
+		{ "IOS", "copy your iOS RevMob Media ID here" }
+	};
+	private RevMob revmob;
+
+	void Awake() {
+		revmob = RevMob.Start(REVMOB_APP_IDS, "AdsRevMob");
+	}
+
+	void Start() {
+		#if UNITY_ANDROID || UNITY_IPHONE
+		RevMobBanner banner = revmob.CreateBanner(RevMob.Position.BOTTOM, 0, 4, 1080, 216);
+		banner.Show();
+		#endif
+	}
+}
